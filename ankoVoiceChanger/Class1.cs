12df31f0970b12cf48b9501234791683b52d26bc
Teruteru <teruterubouzu1024@gmail.com>
2020-08-08 14:42:13 +0900

    プロジェクト ファイルを追加します。

---
+++ ankoVoiceChanger/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ankoVoiceChanger
+{
+    public class Class1
+    {
+    }
+}
